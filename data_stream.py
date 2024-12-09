2e695e417a3fc46bdcb87eaed10667d372bc0575
katek28 <k.korotina28@gmail.com>
2024-12-09 23:06:35 +0100

    Путь к файлу в data_stream

--- data_stream.py
+++ data_stream.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import time
-from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parent
-DATA_PATH = BASE_DIR / 'data' / 'portugal_listings.csv'  
-
-def data_stream(file_path=DATA_PATH, delay=1, chunk_size=10):
+def data_stream(df, delay=1, chunk_size=10):
     
-    df = pd.read_csv(file_path)
+    #df = pd.read_csv(file_path)
     
     
     for i in range(0, len(df), chunk_size):
