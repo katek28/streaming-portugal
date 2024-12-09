b770313d213eb22d3ac2ffdc78eb8ea78d1605be
katek28 <k.korotina28@gmail.com>
2024-12-09 22:42:43 +0100

    Исправлен путь к файлу в data_stream

--- data_stream.py
+++ data_stream.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import time
+from pathlib import Path
 
-def data_stream(file_path, delay=1, chunk_size=10):
-   
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_PATH = BASE_DIR / 'data' / 'portugal_listings.csv'  
+
+def data_stream(file_path=DATA_PATH, delay=1, chunk_size=10):
+    
     df = pd.read_csv(file_path)
+    
+    
     for i in range(0, len(df), chunk_size):
         chunk = df.iloc[i:i + chunk_size]
         yield chunk
-        time.sleep(delay)
+        time.sleep(delay)  
